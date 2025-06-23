b2b6a4df33843693d93fb7396c4f420fa9cba843
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-06-23 18:33:55 +0530

    add Companion Form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,10 @@
+import CompanionForm from "@/components/CompanionForm";
+
 export default function NewCompanion(){
     return (
-        <div>Add New Companion</div>
+        <main className={'min-lg:w-1/2 min-md:w-2/3 items-center justify-center mb-8'}>
+            <h1>Companion Builder</h1>
+            <CompanionForm />
+        </main>
     );
 }
