7dfae33d9db5878d13296e72ac750c85764ba4c4
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-06-23 18:33:09 +0530

    add clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
     variable: "--font-bricolage",
@@ -21,13 +22,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${bricolage.variable} antialiased`}
-      >
-      <Navbar/>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${bricolage.variable} antialiased`}
+        >
+        <Navbar/>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
